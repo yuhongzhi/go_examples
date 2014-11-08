43d645f120411e07aaca3a37ff4016fdc4292c33
公琰 <hongzhi.yuhz@alibaba-inc.com>
2014-11-09 00:06:46 +0800

    Add some examples

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
 	go build -o bin/$(basename ${SRC}) ${SRC}
+clean:
+	rm -rf bin/*
--- basic-types.go
+++ basic-types.go
@@ -9,10 +9,12 @@ var (
     ToBe   bool       = false
     MaxInt uint64     = 1<<64 - 1
     z      complex128 = cmplx.Sqrt(-5 + 12i)
+    z2     = -1 + 2i
 )
 func main() {
     const f = "%T(%v)\n"
     fmt.Printf(f, ToBe, ToBe)
     fmt.Printf(f, MaxInt, MaxInt)
     fmt.Printf(f, z, z)
+    fmt.Printf(f, z2, z2)
 }
